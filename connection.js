f07e16b3eaf31caff1143c97bb13f7b67c6a98d1
Noah <noahejler@gmail.com>
2021-04-09 08:49:16 +0200

    Update connection.js

--- connection.js
+++ connection.js
@@ -5,7 +5,7 @@ async function main(){
      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
-    const uri = "mongodb+srv://Admin:<123>@roulette.x4gkd.mongodb.net/sample_airbnb?retryWrites=true&w=majority"
+    const uri = "mongodb+srv://Admin:<123>@roulette.x4gkd.mongodb.net/test?retryWrites=true&w=majority"
 
     const client = new MongoClient(uri);
  
