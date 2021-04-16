fc6459599326f9f1367489678d909d70abd9535e
Emiltlarsen <emil.t.larsen@gmail.com>
2021-04-16 11:00:51 +0200

    værsgo noah

--- script.js
+++ script.js
@@ -16,28 +16,67 @@ function setup() {
 
 var rotateHjul = true;
 var rotateBold = true;
-var degree = 0;
-var degree1 = 0;
-var hjulSpeed = 1;
-var boldSpeed = -10;
+var hjulDegree = 0;
+var boldDegree = -3.6;
+var hjulSpeed = 0;
+var boldSpeed = 0;
+
+//(-3.6 < nrElleve < -3.6 + 360/3
+//(-3.6 + (360/37)*2 < nr30 < -3.6 + (360/37)*2
+//(-3.6 + (360/37)*3 < nr30 < -3.6 + (360/37)*3
+//(-3.6 + (360/37)*4 < nr30 < -3.6 + (360/37)*4
+//(-3.6 + (360/37)*5 < nr30 < -3.6 + (360/37)*5
+//(-3.6 + (360/37)*6 < nr30 < -3.6 + (360/37)*6
+//(-3.6 + (360/37)*7 < nr30 < -3.6 + (360/37)*7
+//(-3.6 + (360/37)*8 < nr30 < -3.6 + (360/37)*8
+//(-3.6 + (360/37)*9 < nr30 < -3.6 + (360/37)*9
+//(-3.6 + (360/37)*10 < nr30 < -3.6 + (360/37)*10
+//(-3.6 + (360/37)*11 < nr30 < -3.6 + (360/37)*11
+//(-3.6 + (360/37)*12 < nr30 < -3.6 + (360/37)*12
+//(-3.6 + (360/37)*13 < nr30 < -3.6 + (360/37)*13
+//(-3.6 + (360/37)*14 < nr30 < -3.6 + (360/37)*14
+//(-3.6 + (360/37)*15 < nr30 < -3.6 + (360/37)*15
+//(-3.6 + (360/37)*16 < nr30 < -3.6 + (360/37)*16
+//(-3.6 + (360/37)*17 < nr30 < -3.6 + (360/37)*17
+//(-3.6 + (360/37)*18 < nr30 < -3.6 + (360/37)*18
+//(-3.6 + (360/37)*19 < nr30 < -3.6 + (360/37)*19
+//(-3.6 + (360/37)*20 < nr30 < -3.6 + (360/37)*20
+//(-3.6 + (360/37)*21 < nr30 < -3.6 + (360/37)*21
+//(-3.6 + (360/37)*22 < nr30 < -3.6 + (360/37)*22
+//(-3.6 + (360/37)*23 < nr30 < -3.6 + (360/37)*23
+//(-3.6 + (360/37)*24 < nr30 < -3.6 + (360/37)*24
+//(-3.6 + (360/37)*25 < nr30 < -3.6 + (360/37)*25
+//(-3.6 + (360/37)*26 < nr30 < -3.6 + (360/37)*26
+//(-3.6 + (360/37)*27 < nr30 < -3.6 + (360/37)*27
+//(-3.6 + (360/37)*28 < nr30 < -3.6 + (360/37)*28
+//(-3.6 + (360/37)*29 < nr30 < -3.6 + (360/37)*29
+//(-3.6 + (360/37)*30 < nr30 < -3.6 + (360/37)*30
+//(-3.6 + (360/37)*31 < nr30 < -3.6 + (360/37)*31
+//(-3.6 + (360/37)*32 < nr30 < -3.6 + (360/37)*32
+//(-3.6 + (360/37)*33 < nr30 < -3.6 + (360/37)*33
+//(-3.6 + (360/37)*34 < nr30 < -3.6 + (360/37)*34
+//(-3.6 + (360/37)*35 < nr30 < -3.6 + (360/37)*35
+//(-3.6 + (360/37)*36 < nr30 < -3.6 + (360/37)*36
+//(-3.6 + (360/37)*0 < nr30 < -3.6 + (360/37)*0
+
 
 
 function rotateTheHjul() {
-    if (rotateHjul === true && degree === 360-hjulSpeed) {
-        degree = 0;
+    if (rotateHjul === true && hjulDegree === 360-hjulSpeed) {
+        hjulDegree = 0;
     } else if (rotateHjul === true){
-        degree += hjulSpeed;
+        hjulDegree += hjulSpeed;
     }
-    rotate(degree); 
+    rotate(hjulDegree); 
 }
 
 function rotateTheBold() {
-    if (rotateBold === true && degree === 360-boldSpeed) {
-        degree1 = 0;
+    if (rotateBold === true && boldDegree <= -360+boldSpeed) {
+        boldDegree = 0;
     } else if (rotateBold === true) {
-        degree1 += boldSpeed;
+        boldDegree += boldSpeed;
     }
-    rotate(degree1); 
+    rotate(boldDegree); 
 }
 
 /*
@@ -65,6 +104,11 @@ class kugle {
 let KugleBord = new kugle(canvasW/2, canvasH/2, 10, 100, 'blue')
 */
 
+function startGame() {
+    countdoen(10);
+    state = 0;
+}
+
 function countdown(input) {
     countDownStatus = true;
     timeLeft = input;
@@ -75,18 +119,16 @@ function countdown(input) {
 }
 
 function hastighedModi() {
-    if (state === 1){
-        hjulSpeed = -10;
-
+    if (state === 0){
+        boldSpeed = -10;
+    }   else if (state === 1){
+        boldSpeed += 0.05;
     }   else if (state === 2){
-        hjulSpeed = 1;
-        
+        boldSpeed = 0;
     }   else if (state === 3){
 
 
     }
-
-
 }
 
 
@@ -102,6 +144,10 @@ function draw() {
     translate(0,0);
     rotateTheBold();
     circle(radius, radius, 15);
+
+    //console.log(boldDegree);
+    console.log(hjulDegree - boldDegree);
+
 
     if(countDownStatus === true) {
         timeLeft - 1/60;
