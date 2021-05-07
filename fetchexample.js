d2e9b8351c2c8393669c8518c7e604cbe516b0d7
Ejler2 <57224628+Ejler2@users.noreply.github.com>
2021-05-07 18:58:16 +0200

    Major server update

--- fetchexample.js
+++ fetchexample.js
@@ -1,26 +1,48 @@
 let url = "mongodb://localhost:27017/";
 
+GetUser = Userlog;
+Bankupdate = 0;
+
+//ny bruger
 function insert() {
   fetch(url + "/insert/" + prompt("name") + "/" + prompt("code"))
   .then(response => response.json())
   .then(data => alert("ny bruger" + JSON.stringify(data)));
-  console.log("name" + " " + "code");
+  console.log(data);
 }
 
+//Login af bruger
+function Login() {
+  fetch(url + "/insert/" + prompt("name") + "/" + prompt("code"))
+  .then(response => response.json())
+  .then(data => alert(JSON.stringify(data)));
+}
+
+//Finder alle brugere (Admin ting)
 function findAll() {
   fetch(url + "/findAll")
   .then(response => response.json())
   .then(data => alert(JSON.stringify(data)));
 }
 
-function del() {
-  fetch(url + "/delete/" + prompt("Name"))
+//Fjerner bruger
+function Fjern() {
+  fetch(url + "/delete/" + prompt("name") + "/" + prompt("code"))
   .then(response => response.json())
   .then(data => alert("Deleted: " + JSON.stringify(data)));
 }
 
+//Opdatere banken
 function update() {
-  fetch(url + "/update/" + prompt("Name") + "/" + prompt("Score"))
+  fetch(url + "/update/" + Bankupdate)
   .then(response => response.json())
   .then(data => alert("Updated score: " + JSON.stringify(data)));
 }
+
+//Opdatere banken
+function Bankinfo() {
+  fetch(url + "/Sebank/")
+  .then(response => response.json())
+  .then(data => alert("bank: " + JSON.stringify(data)));
+}
+
